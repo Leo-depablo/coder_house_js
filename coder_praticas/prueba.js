5409fadb2619692c86145a6fba37ebdfa307a68c
Leo-Depablo <leodavincci2004@gmail.com>
2025-03-15 11:25:31 -0400

    agrego los archivos iniciales

---
+++ coder_praticas/prueba.js
@@ -0,0 +1,12 @@
+let prueba = 2;
+
+if (prueba > 0) {
+  while (prueba < 100) {
+    console.log(prueba);
+    prueba++;
+  }
+} else {
+  for (const prueba = 0; prueba <= 5; prueba++) {
+    console.log(prueba);
+  }
+}
